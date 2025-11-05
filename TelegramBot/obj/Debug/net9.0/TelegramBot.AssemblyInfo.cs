9545ab5bfb1391606acf13503120b3fabded26f9
khalafi <bahman.esh@gmail.com>
2025-11-05 16:17:56 +0330

    تنظیم سیستم

--- TelegramBot/obj/Debug/net9.0/TelegramBot.AssemblyInfo.cs
+++ TelegramBot/obj/Debug/net9.0/TelegramBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a2aa95ee57f60edf9e966936ef29bffc909b27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+696fb1aaa11e4098cb790a396d2135306ee3ead6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
